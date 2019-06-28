94e33f159b18cdf918e1dd8aea7a9d9d96dc621a
roselandroche <r.landroche@gmail.com>
2019-06-28 19:56:30 -0400

    Working on closure.js Challenge 3

--- assignments/closure.js
+++ assignments/closure.js
@@ -46,7 +46,7 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-  return upDown = {
+  return objCount = {
     increment: function(num1) {
       let countUp = num1;
       return function() {
