1d1c7562318bffab9f4dedc34ebb886065068628
roselandroche <r.landroche@gmail.com>
2019-06-28 19:35:48 -0400

    Completed challenge 2 in closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -24,7 +24,19 @@ groundFloor();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    count += 1;
+    return count;
+  }
 };
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -34,4 +46,5 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
 };
